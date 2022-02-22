ff3c3a9ba84875ef0328aa3db02a894ac0556fa3
fawadali1122 <fawadali12345@yahoo.com>
2022-02-22 19:20:54 +0500

    Complete MERN Stack App

---
+++ server/server.js
@@ -0,0 +1,33 @@
+//https://dev.to/mritunjaysaha/mern-stack-todo-application-backend-282a
+/// server.js
+require("dotenv").config(); //added
+const express = require("express");
+const cors = require("cors"); // added
+
+const connectDB = require("./config/db"); //added
+
+const app = express();
+
+// routes
+const todo = require("./routers/todo"); 
+
+
+// connect database
+connectDB(); //added
+
+// cors
+app.use(cors({ origin: true, credentials: true })); // added
+
+// initialize middleware
+app.use(express.json({ extended: false }));
+app.get("/", (req, res) => res.send("Server up and running"));
+
+// use routes
+app.use("/api/todo", todo); // added
+
+// setting up port
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+    console.log(`server is running on http://localhost:${PORT}`);
+});
